46f66291645f73dd213d09477aa0fd9d06549a3b
tjesser <tjesser@ucdavis.edu>
2016-10-05 12:30:49 -0700

    Add simple tests.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn simple_math() {
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+#[should_panic(expected = "assertion failed")]
+fn newspeak_math() {
+    assert_eq!(2 + 2, 5);
+}
